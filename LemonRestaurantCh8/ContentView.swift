4e564820b7f13779cb25be56d98ed2a1f6bc9fbd
ctwohofakit <ctwohofa@gmail.com>
2026-03-18 22:28:47 -0700

    Initial Commit

---
+++ LemonRestaurantCh8/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  LemonRestaurantCh8
+//
+//  Created by Kit Sitou on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Image(systemName: "sun.min.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.yellow)
+                Text("Monday")
+            }
+            HStack{
+                Image(systemName: "cloud.rain.fill")
+                    .imageScale(.small)
+                    .foregroundStyle(.purple)
+                Text("Tuesday")
+                    .bold()
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LemonRestaurantCh8/LemonRestaurantCh8App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantCh8App.swift
+//  LemonRestaurantCh8
+//
+//  Created by Kit Sitou on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantCh8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
