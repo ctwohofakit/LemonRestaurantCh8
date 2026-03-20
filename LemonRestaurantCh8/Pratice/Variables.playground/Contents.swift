f6318bad935f90c7d91570c5b85cec10a08d2cae
kit sitou <ctwohofa@gmail.com>
2026-03-19 18:18:47 -0700

    about us

--- LemonRestaurantCh8/Pratice/Variables.playground/Contents.swift
+++ LemonRestaurantCh8/Pratice/Variables.playground/Contents.swift
@@ -14,3 +14,49 @@ print("Now we are called \(restaurantName)")
 let city = "Chicago"
 
 print("Our restaurant is located in \(city)")
+
+//string
+var specialDish = "Pasta"
+//integer
+var availableTables = 5
+//Double
+var dishPrice = 12.50
+//booleans
+var isOpen = true
+
+print("Today's special dish \(specialDish) - $\(dishPrice)")
+
+//mini challenge 1
+var numberOfTables = 1
+let ownerName = "Kit"
+print("Little Lemon hsa \(numberOfTables) tables, owned by \(ownerName).")
+
+
+//mini challenge 2
+var veganDish = "Tofu"
+var qty = 5
+var veganPrice = 16.00
+var isPopular = false
+print("One of the dish is \(veganDish), the customer ordered \(qty) and the price is $\(veganPrice). but is it popular? \(isPopular).")
+
+//we can define the type(optional)
+var antherDish: String="Pizza"
+var tableCount:Int=10
+var price:Double=8.99
+var openStatus:Bool=false
+
+var name = "Kit"
+
+let pastaPrice=10.50
+let saladPrice=6.25
+let total = pastaPrice + saladPrice
+print("Total price: $\(total)")
+
+let tip=total*15/100
+let final=tip+total
+print("the final price is: \(final)")
+print("the final price is: \(total+tip)")
+
+
+
+
---
+++ LemonRestaurantCh8/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestaurantCh8
+//
+//  Created by Kit Sitou on 3/19/26.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
